844627d94ff4721c74c1f11af767e325fd87eb38
Jinay777 <jinayjain72@gmail.com>
2023-06-30 15:27:34 +0530

    add emojis

--- src/Components/Emoji.tsx
+++ src/Components/Emoji.tsx
@@ -1,22 +1,20 @@
-import bullsEye from "../assets/bulls-eye.webp"
-import thumbsUp from "../assets/thumbs-up.webp"
-import meh from "../assets/meh.webp"
+import bullsEye from "../assets/bulls-eye.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
 import { Image, ImageProps } from "@chakra-ui/react";
 
-interface props{
-    rating:number;
+interface props {
+  rating: number;
 }
 
-const Emoji= ({rating}:props) => {
-    if (rating<3) return null;
-    const emojiMap:{[key:number]:ImageProps} ={
-        3: {src:meh,alt:"meh",boxSize: "25px"},
-        4: {src:thumbsUp,alt:"Recommended",boxSize: "25px"},
-        5: {src:bullsEye,alt:"Exceptional",boxSize: "35px"},
-    }
-  return (
-    <Image {...emojiMap[rating]} marginTop={1}/>
-  )
-}
+const Emoji = ({ rating }: props) => {
+  if (rating < 3) return null;
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "Recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "Exceptional", boxSize: "35px" },
+  };
+  return <Image {...emojiMap[rating]} marginTop={1} />;
+};
 
-export default Emoji
+export default Emoji;
