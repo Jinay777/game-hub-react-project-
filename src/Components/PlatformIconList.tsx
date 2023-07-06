568ef4e4ad306dddf6a68ab6e9f4ca57826c4bb5
Jinay777 <jinayjain72@gmail.com>
2023-07-06 20:51:25 +0530

    refactor- move entities interface

--- src/Components/PlatformIconList.tsx
+++ src/Components/PlatformIconList.tsx
@@ -6,10 +6,10 @@ import {
   FaPlaystation,
   FaAndroid,
 } from "react-icons/fa";
-import {MdPhoneIphone} from "react-icons/md";
-import {SiNintendo} from "react-icons/si";
-import {BsGlobe} from "react-icons/bs";
-import { Platform } from "../hooks/usePlatforms";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../entities/Platform";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
@@ -18,21 +18,21 @@ interface props {
 }
 
 const PlatformIconList = ({ platforms }: props) => {
-  const iconMap:{[key:string]: IconType } = {
+  const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
     nintendo: SiNintendo,
     mac: FaApple,
-    android:FaAndroid,
+    android: FaAndroid,
     linux: FaLinux,
     ios: MdPhoneIphone,
     web: BsGlobe,
-  }
+  };
   return (
     <HStack marginY="10px">
       {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500"/>
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
