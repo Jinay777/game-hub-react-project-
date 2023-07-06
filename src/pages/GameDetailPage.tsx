cbfc43454ba87f15a41a049898ed629c72aa68d3
Jinay777 <jinayjain72@gmail.com>
2023-07-06 22:34:57 +0530

    Build Game Trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -6,6 +6,7 @@ import ExpandableText from "../Components/ExpandableText";
 import DefinationItem from "../Components/DefinationItem";
 import CriticScore from "../Components/CriticScore";
 import GameAttributes from "../Components/GameAttributes";
+import GameTrailer from "../Components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -17,6 +18,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game}/>
+      <GameTrailer gameId={game.id}/>
     </>
   );
 };
